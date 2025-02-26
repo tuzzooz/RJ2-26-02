c67da2325e724ebc8d1cb1070a5420f2cea9982c
Otavio Vianna <otavioviannalima@gmail.com>
2025-02-26 20:02:07 -0300

    Update RJ2.js

--- RJ2.js
+++ RJ2.js
@@ -1,4 +1,5 @@
 class Cliente {
+    // em um vídeo que assisti, vi que é bom se criar o hábito de usar o construtor na criação de objetos para evitar repetições no código
     constructor(nome, idade, telefone, endereco) {
         this.nome = nome;
         this.idade = idade;
@@ -6,10 +7,20 @@ class Cliente {
         this.endereco = endereco;
     }
 
+    getNomeMaiusculo() {
+        return this.nome.toUpperCase();
+    }
+
+    getNomeMinusculo() {
+        return this.nome.toLowerCase();
+    }
+
     getInfoFormatada() {
         return `--------------------\nInformações do Cliente\n${this.nome}\n--------------------\n\nTelefone:\nDDD: ${this.telefone.ddd}\nNúmero: ${this.telefone.numero}\n\n--------------------\nEndereço:\nRua: ${this.endereco.rua}\nNúmero: ${this.endereco.numero}\nCidade: ${this.endereco.cidade}\nEstado: ${this.endereco.estado}\n--------------------`;
     }
 }
+
+//classes vão armazenar as informações de cada cliente
 
 class Telefone {
     constructor(ddd, numero) {
@@ -29,9 +40,18 @@ class Endereco {
 
 let clientes = [
     new Cliente("Carlos Conrado Heinz", 30, new Telefone("11", "9999346999"), new Endereco("Av. Paulista", "987", "São Paulo", "SP")),
-    new Cliente("Ana Luiza Lima", 25, new Telefone("21", "9834543588"), new Endereco("Rua das Flores", "123", "Rio de Janeiro", "RJ")),
-    new Cliente("Beatriz Itoshi", 22, new Telefone("31", "97643634637"), new Endereco("Rua Central", "456", "Belo Horizonte", "MG"))
+    new Cliente("Ana Luiza Lima", 25, new Telefone("62", "9834543588"), new Endereco("Rua das Flores", "123", "Rio de Janeiro", "RJ")),
+    new Cliente("Beatriz Vianna", 22, new Telefone("45", "97643634637"), new Endereco("Rua Central", "456", "Belo Horizonte", "MG"))
 ];
+
+//usando o sort e localcompare para ordenar os clientes
 
 clientes.sort((a, b) => a.nome.localeCompare(b.nome))
         .forEach(cliente => console.log(cliente.getInfoFormatada()));
+
+
+
+//alterne de 0 a 2 para conseguir o nome do cliente que desejar em maiusculo ou minusculo
+console.log(clientes[2].getNomeMaiusculo()); 
+console.log(clientes[0].getNomeMinusculo()); 
+        
